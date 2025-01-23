bea70c2bb522cc6c4428bf66b1e950aa55653237
skythelimitdt <eylemy2020@gmail.com>
2025-01-22 18:00:32 -0700

    cleaned up and updated files

--- schema.sql
+++ schema.sql
@@ -1,12 +1,13 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-DROP TABLE "Damage";
-DROP TABLE "Classification" CASCADE;
-DROP TABLE "Locations" CASCADE;
-DROP TABLE "Events" CASCADE;
-DROP TABLE "Affected" CASCADE;
+﻿DROP TABLE damage CASCADE;
+DROP TABLE classification CASCADE;
+DROP TABLE locations CASCADE;
+DROP TABLE events CASCADE;
+DROP TABLE affected CASCADE;
+DROP TABLE tropicalcyclone CASCADE;
 
-CREATE TABLE "Damage" (
+
+
+CREATE TABLE "damage" (
     "ImpactID" VARCHAR(255)   NOT NULL,
     "DisNo" VARCHAR(255)   NOT NULL,
     "Classification_Key" VARCHAR(100)   NOT NULL,
@@ -15,36 +16,32 @@ CREATE TABLE "Damage" (
     "Start_Year" INTEGER   NOT NULL,
     "Total_Damage_USD" FLOAT   NULL,
     "Total_Damage_Adj_USD" FLOAT   NULL,
-    CONSTRAINT "pk_Damage" PRIMARY KEY (
+    CONSTRAINT "pk_damage" PRIMARY KEY (
         "ImpactID"
      )
 );
 
-CREATE TABLE "Affected" (
+CREATE TABLE "affected" (
     "ImpactID" VARCHAR(255)   NOT NULL,
     "LocationID" VARCHAR(255)   NOT NULL,
     "Classification_Key" VARCHAR(100)   NOT NULL,
+    "DisNo" VARCHAR(255)   NOT NULL,
     "ISO" VARCHAR(3)   NOT NULL,
     "Total_Deaths" FLOAT   NULL,
     "No_Injured" FLOAT   NULL,
     "No_Affected" FLOAT   NULL,
     "No_Homeless" FLOAT   NULL,
     "Total_Affected" FLOAT   NULL,
-    CONSTRAINT "pk_Affected" PRIMARY KEY (
+    CONSTRAINT "pk_affected" PRIMARY KEY (
         "ImpactID"
      )
 );
 
-CREATE TABLE "Events" (
+CREATE TABLE "events" (
     "DisNo" VARCHAR(255)   NOT NULL,
     "ImpactID" VARCHAR(255)   NOT NULL,
     "Historic" VARCHAR(250)   NOT NULL,
     "Classification_Key" VARCHAR(100)   NOT NULL,
-    "Disaster_Group" VARCHAR(255)   NOT NULL,
-    "Disaster_Subgroup" VARCHAR(255)   NOT NULL,
-    "Disaster_Type" VARCHAR(255)   NOT NULL,
-    "Disaster_Subtype" VARCHAR(255)   NOT NULL,
-    "Event_Name" VARCHAR(255)   NULL,
     "OFDA_BHA_Response" VARCHAR(3)   NOT NULL,
     "Appeal" VARCHAR(3)   NOT NULL,
     "Declaration" VARCHAR(3)   NOT NULL,
@@ -55,68 +52,82 @@ CREATE TABLE "Events" (
     "Start_Date" DATE   NOT NULL,
     "End_Date" DATE   NOT NULL,
     "duration" INTEGER   NOT NULL,
-    CONSTRAINT "pk_Events" PRIMARY KEY (
+    CONSTRAINT "pk_events" PRIMARY KEY (
         "DisNo"
-     ),
-    CONSTRAINT "uc_Events_ImpactID" UNIQUE (
-        "ImpactID"
-    )
+     )
 );
 
-CREATE TABLE "Locations" (
+CREATE TABLE "locations" (
     "LocationID" VARCHAR(255)   NOT NULL,
     "DisNo" VARCHAR(255)   NOT NULL,
     "Classification_Key" VARCHAR(100)   NOT NULL,
     "ISO" VARCHAR(3)   NOT NULL,
-    "Country" VARCHAR(255)   NOT NULL,
-    "Subregion" VARCHAR(255)   NOT NULL,
-    "Region" VARCHAR(255)   NOT NULL,
     "Location" VARCHAR(3000)   NULL,
-    "Latitude" FLOAT   NULL,
-    "Longitude" FLOAT   NULL,
     "River_Basin" VARCHAR(500)   NULL,
-    CONSTRAINT "pk_Locations" PRIMARY KEY (
+    CONSTRAINT "pk_locations" PRIMARY KEY (
         "LocationID"
      ),
-    CONSTRAINT "uc_Locations_DisNo" UNIQUE (
+    CONSTRAINT "uc_locations_DisNo" UNIQUE (
         "DisNo"
     )
 );
 
-CREATE TABLE "Classification" (
+CREATE TABLE "classification" (
     "Classification_Key" VARCHAR(100)   NOT NULL,
     "Disaster_Group" VARCHAR(255)   NOT NULL,
     "Disaster_Subgroup" VARCHAR(255)   NOT NULL,
     "Disaster_Type" VARCHAR(255)   NOT NULL,
     "Disaster_Subtype" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Classification" PRIMARY KEY (
+    CONSTRAINT "pk_classification" PRIMARY KEY (
         "Classification_Key"
      )
 );
 
-SELECT * FROM "Affected";
+CREATE TABLE "tropicalcyclone" (
+    "DisNo" VARCHAR(255)   NOT NULL,
+    "ImpactID" VARCHAR(255)   NOT NULL,
+    "LocationID" VARCHAR(255)   NOT NULL,
+    "Classification_Key" VARCHAR(100)   NOT NULL,
+    "Event_Name" VARCHAR(255)   NOT NULL,
+    "ISO" VARCHAR(3)   NOT NULL,
+    "Total_Deaths" FLOAT   NULL,
+    "Total_Affected" FLOAT   NULL,
+    "Start_Date" DATE   NOT NULL,
+    "Total_Damage_USD" FLOAT   NULL,
+    "Total_Damage_Adj_USD" FLOAT   NULL,
+    "duration" INTEGER   NOT NULL,
+    CONSTRAINT "pk_tropicalcyclone" PRIMARY KEY (
+        "ImpactID"
+     ),
+    CONSTRAINT "uc_tropicalcyclone_DisNo" UNIQUE (
+        "DisNo"
+    )
+);
 
-ALTER TABLE "Damage" ADD CONSTRAINT "fk_Damage_Classification_Key" FOREIGN KEY("Classification_Key")
-REFERENCES "Classification" ("Classification_Key");
+ALTER TABLE "damage" ADD CONSTRAINT "fk_damage_Classification_Key" FOREIGN KEY("Classification_Key")
+REFERENCES "classification" ("Classification_Key");
 
-ALTER TABLE "Damage" ADD CONSTRAINT "fk_Damage_LocationID" FOREIGN KEY("LocationID")
-REFERENCES "Locations" ("LocationID");
+ALTER TABLE "damage" ADD CONSTRAINT "fk_damage_LocationID" FOREIGN KEY("LocationID")
+REFERENCES "locations" ("LocationID");
 
-ALTER TABLE "Affected" ADD CONSTRAINT "fk_Affected_LocationID" FOREIGN KEY("LocationID")
-REFERENCES "Locations" ("LocationID");
+ALTER TABLE "affected" ADD CONSTRAINT "fk_affected_LocationID" FOREIGN KEY("LocationID")
+REFERENCES "locations" ("LocationID");
 
-ALTER TABLE "Affected" ADD CONSTRAINT "fk_Affected_Classification_Key" FOREIGN KEY("Classification_Key")
-REFERENCES "Classification" ("Classification_Key");
+ALTER TABLE "affected" ADD CONSTRAINT "fk_affected_Classification_Key" FOREIGN KEY("Classification_Key")
+REFERENCES "classification" ("Classification_Key");
 
-ALTER TABLE "Events" ADD CONSTRAINT "fk_Events_ImpactID" FOREIGN KEY("ImpactID")
-REFERENCES "Affected" ("ImpactID");
+ALTER TABLE "affected" ADD CONSTRAINT "fk_affected_DisNo" FOREIGN KEY("DisNo")
+REFERENCES "events" ("DisNo");
 
-ALTER TABLE "Events" ADD CONSTRAINT "fk_Events_Classification_Key" FOREIGN KEY("Classification_Key")
-REFERENCES "Classification" ("Classification_Key");
+ALTER TABLE "events" ADD CONSTRAINT "fk_events_ImpactID" FOREIGN KEY("ImpactID")
+REFERENCES "damage" ("ImpactID");
 
-ALTER TABLE "Locations" ADD CONSTRAINT "fk_Locations_DisNo" FOREIGN KEY("DisNo")
-REFERENCES "Events" ("DisNo");
+ALTER TABLE "events" ADD CONSTRAINT "fk_events_Classification_Key" FOREIGN KEY("Classification_Key")
+REFERENCES "classification" ("Classification_Key");
 
-ALTER TABLE "Locations" ADD CONSTRAINT "fk_Locations_Classification_Key" FOREIGN KEY("Classification_Key")
-REFERENCES "Classification" ("Classification_Key");
+ALTER TABLE "tropicalcyclone" ADD CONSTRAINT "fk_tropicalcyclone_DisNo" FOREIGN KEY("DisNo")
+REFERENCES "events" ("DisNo");
+
+ALTER TABLE "tropicalcyclone" ADD CONSTRAINT "fk_tropicalcyclone_LocationID" FOREIGN KEY("LocationID")
+REFERENCES "locations" ("LocationID");
 
