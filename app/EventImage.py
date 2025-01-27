48e556ee87a242c155d7464cfd857276351bb1de
Ian OConnor <ianoconnor1217@gmail.com>
2025-01-27 11:06:55 -0800

    update

---
+++ app/EventImage.py
@@ -0,0 +1,26 @@
+def flood_image():
+    pass
+
+def fire_image():
+    pass
+
+def earthquake_image():
+    pass
+
+def volcano_image():
+    pass
+
+def storm_image():
+    pass
+
+def drought_image():
+    pass
+
+def x_image():
+    pass
+
+def y_image():
+    pass
+
+def z_image():
+    pass
--- app/Splash.py
+++ app/Splash.py
@@ -14,18 +14,18 @@ class Splash:
         # Title label
         ttk.Label(
             self.main_frame,
-            text="Welcome to the 2000s Disaster Information App",
+            text="Welcome to the Natural Disaster Information App",
             font=("Arial", 14, "bold"),
         ).grid(row=0, column=0, columnspan=2, pady=(0, 20))
 
         ttk.Label(
             self.main_frame,
-            text="Looking back we had some pretty great natural disasters...",
+            text="Maybe lets say something about disasters or climate change here?",
             font=("Arial", 10),
         ).grid(row=1, column=0, columnspan=2, pady=(0, 15))
 
         # Load and display the image
-        self.image = self.image = tk.PhotoImage(file=r"C:\Users\Ian O'Connor\Github\natural-disaster-insights\app\resources\splash.png")
+        self.image = self.image = tk.PhotoImage(file=r"")
         self.image_label = tk.Label(self.main_frame, image=self.image)
         self.image_label.grid(row=2, column=0, columnspan=2, pady=(0, 20))
 
